7cadc2c11129779b18b0e924d3582783343fe2ef
pashech <pashech555@gmail.com>
2023-03-24 00:07:44 +0300

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,19 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
+
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        List<String> temp = new ArrayList<>();
+        for(String s : args){
+            if(StringUtils.isPositiveNumber(s)){
+                temp.add(s);
+            }
+        }
+        return temp.size() == args.size();
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        int number = Integer.parseInt(org.apache.commons.lang3.StringUtils.getDigits(str));
+        return number > 0;
     }
 }
