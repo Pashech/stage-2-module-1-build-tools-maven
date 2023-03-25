001ed901d38345a606d27e2e2be082029774f72b
pashech <pashech555@gmail.com>
2023-03-25 14:06:30 +0300

    done

---
+++ demo/src/test/java/com/epam/demo/UtilsTest.java
@@ -0,0 +1,20 @@
+package com.epam.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+class UtilsTest {
+
+
+    @Test
+    void isAllPositiveNumbers() {
+        List<String> trueList = Arrays.asList("1", "2", "4");
+        Assertions.assertTrue(Utils.isAllPositiveNumbers(trueList));
+        List<String> falseList = Arrays.asList("1", "2", "-4");
+        Assertions.assertFalse(Utils.isAllPositiveNumbers(falseList));
+    }
+}
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        int number = Integer.parseInt(org.apache.commons.lang3.StringUtils.getDigits(str));
+        char[] ch;
+        ch = str.toCharArray();
+        String s = org.apache.commons.lang3.StringUtils.valueOf(ch);
+        int number = Integer.parseInt(s);
         return number > 0;
     }
 }
